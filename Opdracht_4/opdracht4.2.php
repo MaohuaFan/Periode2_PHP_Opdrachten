3025ff5bae28188591dca522e8eca418b4756726
MaohuaFan <9019910@student.zadkine.nl>
2022-11-18 13:45:07 +0100

    php

--- Opdracht_4/opdracht4.2.php
+++ Opdracht_4/opdracht4.2.php
@@ -10,18 +10,18 @@
     <?php
             $uur = date("H");
             switch($uur) {
-                case ($uur < 6):
-                    echo"Het is nacht";
-                    break;
-                case ($uur < 12):
+                case ($uur > 6 && $uur < 12):
                     echo"Het is octend";
                     break;
-                case ($uur < 18):
+                case ($uur > 12 && $uur < 18):
                     echo"Het is middag";
                     break;
-                case ($uur < 24):
+                case ($uur > 18 && $uur < 24):
                     echo"Het is avond";
                     break;
+                    case ($uur > 24 && $uur < 6):
+                        echo"Het is nacht";
+                        break;
             }
     ?>
 </body>
