cfc93e3659c4a610365173326ef9542ed344e30c
MaohuaFan <9019910@student.zadkine.nl>
2022-11-18 12:56:07 +0100

    php

---
+++ Opdracht_3/opdracht3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        #Maak een PHP pagina waarin je 2 string variabelen op het scherm laat zien met echo.
+        #Maak ook 2 variabelen met een getal en laat deze met print op het scherm tonen.
+        
+    ?>
+</body>
+</html>
