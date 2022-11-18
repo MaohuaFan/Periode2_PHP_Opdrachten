53a07bae87d13da81c4552397a3215ba6ca1b16c
MaohuaFan <9019910@student.zadkine.nl>
2022-11-18 13:35:51 +0100

    php

--- Opdracht_4/opdracht4.4.php
+++ Opdracht_4/opdracht4.4.php
@@ -10,8 +10,8 @@
     <?php
         $product = 55;
         if ($product > 150){
-        $product = $product * 1.19;
-        echo "De nieuwe prijs is $product";
+            $product = $product * 1.19;
+            echo "De nieuwe prijs is $product";
         }
         elseif ($product < 55){
             $product = $product * 1.11;
