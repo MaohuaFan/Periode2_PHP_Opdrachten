8e649711e2189aec43f04f34cfbe949e6dd29b72
MaohuaFan <9019910@student.zadkine.nl>
2022-11-23 13:07:48 +0100

    php

--- Opdracht_2/opdracht2b.php
+++ Opdracht_2/opdracht2b.php
@@ -10,9 +10,13 @@
     <h1>Hallo Wereld!</h1>
     <p>Heb nog geen boek</p>
     <?php
-        echo date("l") . "<br>";
-        echo date("l") . "<br>";
-        echo date("l") . "<br>";
+        date_default_timezone_set("Europe/Amsterdam");
+        $today = date("l j F Y");
+        echo "Het is vandaag: $today" . "<br>";
+        echo "Vandaag is het de" . date("XXX") . "van het jaar." . "<br>";
+        echo date("l") . "is de" . date("XXX") . "dag van de week." . "<br>";
+        echo "De maand" . date("l") . "heeft in totaal 31 dagen." . "<br>";
+        echo "Het jaar" . date("l") . "is geen schrikkeljaar" . "<br>";
     ?>
 </body>
 </html>
