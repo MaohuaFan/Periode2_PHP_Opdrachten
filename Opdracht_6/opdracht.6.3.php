145bf37c09fa1fb32faac0f7515b481193eba024
MaohuaFan <9019910@student.zadkine.nl>
2022-12-02 13:13:56 +0100

    php

---
+++ Opdracht_6/opdracht.6.3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        function WillekeurigePostcode() {
+            echo rand(1000,9999);
+            $stringtekst = "ABCEDFGHIJKLMNOPQRSTUVWXYZ";
+            $random = substr(str_shuffle($stringtekst), 0, 2);
+            echo $random;
+        }
+        WillekeurigePostcode();
+    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
