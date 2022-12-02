5f8d4c3136534e70d7f3f931dbb6be7afa6a9341
MaohuaFan <9019910@student.zadkine.nl>
2022-12-02 13:17:20 +0100

    php

--- Opdracht_6/opdracht.6.3.php
+++ Opdracht_6/opdracht.6.3.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <?php
         function WillekeurigePostcode() {
             echo rand(1000,9999);
@@ -10,12 +16,5 @@
         }
         WillekeurigePostcode();
     ?>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
 </body>
 </html>
