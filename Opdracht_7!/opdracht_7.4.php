b1810f5b53102c6b4c50139ee515aab0ee73f757
MaohuaFan <9019910@student.zadkine.nl>
2022-12-09 13:25:10 +0100

    php

--- Opdracht_7!/opdracht_7.4.php
+++ Opdracht_7!/opdracht_7.4.php
@@ -8,21 +8,20 @@
 </head>
 <body>
     <form action="#" method="post">
-    Prijs <input type="number" name="geldbedrag" value="prijs"><br>
-    Korting (%) <input type="number" name="geldbedrag" value="korting">
-    <br><input type="submit" name="uitrekenen" value="Uitrekenen">
+        Prijs <input type="number" name="prijs" value="prijs">
+    <br>Korting (%) <input type="number" name="korting" value="korting">
+    <br><input type="submit" name="uitrekenen" value="Uitrekenen" >
     </form>
 
     <?php
+    if(isset($_POST["uitrekenen"])) {
+        $prijs = $_POST["prijs"];
+        $korting = $_POST["korting"];
+        $som = $prijs * (100 -$korting)/100;
+        echo"Orginele Bedrag: $prijs <br>";
+        echo"Bedrag inclusief $korting% korting: €$som";
+    }
 
-        if(isset($_POST["geldbedrag"])) {
-            function number_formats() {
-            $prijs = $_POST["prijs"];
-            $korting = 100 - $_POST["korting"];
-            $som = $prijs * $korting;
-            echo"Bedrag inclusief $korting% korting: €$som";
-            }
-        }
     ?>
 </body>
 </html>
