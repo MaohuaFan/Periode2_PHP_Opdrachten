a36d33d8c80b80366aa71918d2d8826d55102095
MaohuaFan <9019910@student.zadkine.nl>
2023-01-13 11:58:59 +0100

    php css

--- Opdracht_10/opdracht_9.1.php
+++ Opdracht_10/opdracht_9.1.php
@@ -5,19 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style9.1.css">
 </head>
-<body>
+<body></body>
     <?php
     try {
         $db = new PDO("mysql:host=localhost;dbname=fietsenmaker", "root", "");
         $query = $db->prepare("SELECT * FROM fietsen");
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        foreach($result as &$data) {
-            echo $data["merk"] . "";
-            echo $data["type"] . "";
-            echo $data["prijs"] . "<br>";
-        }
+        echo"<table>";
+            foreach($result as &$data) {
+                echo "<tr>";
+                    echo "<td>" . $data["merk"] . "</td>";
+                    echo "<td>" . $data["type"] . "</td>";
+                    echo "<td>&euro; " . number_format($data["prijs"],2,",",".") . "</td>";
+                echo "</tr>";
+            }
+        echo"</table>";
     } catch (PDOException $e) {
         die("Error!: " . $e->getMessage());
     }
