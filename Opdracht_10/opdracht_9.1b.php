01b4b7d39dd0f4112ccb8c99e5ca42ac26067214
MaohuaFan <9019910@student.zadkine.nl>
2023-01-13 11:52:25 +0100

    php

--- Opdracht_10/opdracht_9.1b.php
+++ Opdracht_10/opdracht_9.1b.php
@@ -1,19 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style9.1b.css">
+</head>
+<body>
+    <?php
     try {
         $db = new PDO("mysql:host=localhost;dbname=fietsenmaker", "root", "");
         $query = $db->prepare("SELECT * FROM fietsen");
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        echo"<table>";
+            foreach($result as &$data) {
+                echo "<tr>";
+                    echo "<td>" . $data["merk"] . "</td>";
+                    echo "<td>" . $data["type"] . "</td>";
+                    echo "<td>" . $data["prijs"] . "</td>";
+                echo "</tr>";
+            }
         echo"</table>";
-        foreach($result as &$data) {
-            echo "<tr>";
-            echo "<td>" . $data["merk"] . "</td>";
-            echo "<td>" . $data["type"] . "</td>";
-            echo "<td>" . $data["prijs"] . "</td>";
-        echo "</tr>";
-        }
-    echo"</table>";
     } catch (PDOException $e) {
         die("Error!: " . $e->getMessage());
     }
-?>
+    ?>
+</body>
+</html>
